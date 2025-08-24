13434658c398e66ea900a9b9a255d634a0324c1a
Ankit <tomarankit78@gmail.com>
2025-08-25 00:02:01 +0530

    Adding API-Methods to Service Classes Repository Interface

--- userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
 
     @GetMapping("/register")
     public ResponseEntity<UserResponse> register(@Valid  @RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(userService.register(request);
+        return ResponseEntity.ok(userService.register(request));
     }
 
 
