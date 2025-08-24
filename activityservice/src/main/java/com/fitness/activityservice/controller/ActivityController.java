8a32d0c455059d2d92f3fb90e01a08dfde6b54b6
Ankit <tomarankit78@gmail.com>
2025-08-25 01:42:02 +0530

    Activity Service Module Added

--- activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
+++ activityservice/src/main/java/com/fitness/activityservice/controller/ActivityController.java
@@ -1,5 +1,6 @@
 package com.fitness.activityservice.controller;
 
+import com.fitness.activityservice.dto.ActivityResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
