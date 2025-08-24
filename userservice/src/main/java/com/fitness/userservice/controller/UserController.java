73f506c367156583e7a4568282c530fa56ed6daf
Ankit <tomarankit78@gmail.com>
2025-08-24 23:25:38 +0530

    Added Dtos and Service packages and Their classes

--- userservice/src/main/java/com/fitness/userservice/controller/UserController.java
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -1,17 +1,28 @@
 package com.fitness.userservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fitness.userservice.dto.RegisterRequest;
+import com.fitness.userservice.dto.UserResponse;
+import com.fitness.userservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/users")
+@AllArgsConstructor
 public class UserController {
 
+    private UserService userService;
+
     @GetMapping("/{users}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
-        return ResponseEntity.ok().body(new User());
+    public ResponseEntity<UserResponse> getUserProfile(@PathVariable String userId) {
+        return ResponseEntity.ok(userService.getUserProfile(UserId));
+    }
+
+    @GetMapping("/register")
+    public ResponseEntity<UserResponse> register(@Valid  @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(request.register(request);
     }
 
 
