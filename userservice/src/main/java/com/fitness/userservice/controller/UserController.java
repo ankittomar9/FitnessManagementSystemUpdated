56165646d1400385a0600e3e7ee72fb79a9c31f1
Ankit <tomarankit78@gmail.com>
2025-08-24 21:30:07 +0530

    Adding yml configuration for application

---
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.fitness.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    @GetMapping("/{users}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(new User());
+    }
+}
