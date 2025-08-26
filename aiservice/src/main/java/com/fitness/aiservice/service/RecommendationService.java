857226f3a8b40ee46379858403755400622f666b
Ankit <tomarankit78@gmail.com>
2025-08-26 19:56:27 +0530

    ai-service controller and Service Methods

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/RecommendationService.java
@@ -0,0 +1,21 @@
+package com.fitness.aiservice.service;
+
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+    private final RecommendationRepository recommendationRepository;
+
+
+    public List<Recommendation> getUserRecommendation(String userId) {
+    }
+
+    public List<Recommendation> getActivityRecommendation(String activityId) {
+    }
+}
