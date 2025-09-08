bc245d2e59e835a50e0dab49d905243ea8b91e0b
tomar <tomarankit78@gmail.com>
2025-09-08 23:41:19 +0530

    All Configurations

--- userservice/src/main/java/com/fitness/userservice/dto/UserResponse.java
+++ userservice/src/main/java/com/fitness/userservice/dto/UserResponse.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 @Data
 public class UserResponse {
     private String userId;
-    private
+    private String
     private String email;
     private String password;
     private  String firstName;
