0f5c19b548e731cfe08020df599d9355e43ca7ef
tomar <tomarankit78@gmail.com>
2025-09-08 20:07:24 +0530

    oAuth Configuration

--- gateway/src/main/java/com/fitness/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/fitness/gateway/SecurityConfig.java
@@ -1,13 +1,23 @@
 package com.fitness.gateway;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+            .csrf().disable()
+            .authorizeExchange()
+            .anyExchange().permitAll();
+        return http.build();
+    }
 
 
 
