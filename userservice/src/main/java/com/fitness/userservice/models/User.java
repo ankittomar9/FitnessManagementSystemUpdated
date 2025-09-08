f9dd8723c26097cbcfc7d9a49f965aefe62d4a4b
tomar <tomarankit78@gmail.com>
2025-09-08 23:40:32 +0530

    All Configurations

--- userservice/src/main/java/com/fitness/userservice/models/User.java
+++ userservice/src/main/java/com/fitness/userservice/models/User.java
@@ -18,7 +18,7 @@ public class User {
     @Column(unique = true, nullable = false)
     private String email;
 
-    private String Key
+    private String keycloakId;
 
     @Column(nullable = false)
     private String password;
