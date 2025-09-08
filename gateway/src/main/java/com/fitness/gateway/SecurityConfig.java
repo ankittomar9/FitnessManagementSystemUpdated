fa3d00e180a8699cdf01d9de999b733ee4de26fe
tomar <tomarankit78@gmail.com>
2025-09-08 21:22:33 +0530

    oAuth Configuration

--- gateway/src/main/java/com/fitness/gateway/SecurityConfig.java
+++ gateway/src/main/java/com/fitness/gateway/SecurityConfig.java
@@ -2,22 +2,27 @@ package com.fitness.gateway;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        http
-            .csrf().disable()
-            .authorizeExchange(exachange -> exachange
-        //        .pathMatchers("/actuator/**").permitAll()
-            .anyExchange().authenticated());
-        return http.build();
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+//                        .pathMatchers("/actuator/*").permitAll()
+                                .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
     }
 
 
