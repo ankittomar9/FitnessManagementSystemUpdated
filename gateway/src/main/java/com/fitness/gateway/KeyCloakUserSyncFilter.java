6073b18e90dc63496a243dc78aa4f34d85d10101
tomar <tomarankit78@gmail.com>
2025-09-09 00:29:10 +0530

    Synchronization Of User

--- gateway/src/main/java/com/fitness/gateway/KeyCloakUserSyncFilter.java
+++ gateway/src/main/java/com/fitness/gateway/KeyCloakUserSyncFilter.java
@@ -1,11 +1,20 @@
 package com.fitness.gateway;
 
+import com.fitness.gateway.user.UserService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class KeyCloakUserSyncFilter {
+    private final UserService userService;
+
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain )
 
 
 
