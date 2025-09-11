2e1d875506dfd87d3bd297060fbfe2c8e3e0ca29
tomar <tomarankit78@gmail.com>
2025-09-11 21:52:37 +0530

    frontend of Fitness Service

--- fitness-app-frontend/src/store/authSlice.js
+++ fitness-app-frontend/src/store/authSlice.js
@@ -1,23 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-
-
-
 const authSlice = createSlice({
-    name: 'counter',
-    initialState,
+    name: 'auth',
+    initialState:{
+        user:JSON.parse(localStorage.getItem('user')) || null,
+        token:localStorage.getItem('token') || null,
+        userId:localStorage.getItem('userId') || null,
+    },
     reducers: {
-        increment(state) {
-            state.value++
+        setCredentials:(state,action) => {
+        state.user=action.payload.user;
+        state.token=action.payload.token;
+        state.userId=action.payload.user.sub;
+        localStorage.setItem('token',action.payload.token);
+        localStorage.setItem('user',JSON.stringify(action.payload.user));
+        localStorage.setItem('userId',action.payload.user.sub);
         },
-        decrement(state) {
-            state.value--
+        logout:(state) =>{
+            state.user=null;
+            state.token=null;
+            state.userId=null;
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+            localStorage.removeItem('userId');
+
+
         },
-        incrementByAmount(state, action: PayloadAction<number>) {
-            state.value += action.payload
-        },
+
     },
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
-export default counterSlice.reducer
+export const { setCredentials,logout} = authSlice.actions;
+export default authSlice.reducer;
