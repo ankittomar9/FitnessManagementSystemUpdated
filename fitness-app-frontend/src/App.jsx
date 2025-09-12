ab7605469b673328662815285e7bf7cb65adde22
tomar <tomarankit78@gmail.com>
2025-09-13 00:22:42 +0530

    frontend of Fitness Service

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -3,10 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter as Router,Navigate,Route,Routes,useLocation}  from "react-router";
-import {Button} from "@mui/material";
+import {Box, Button} from "@mui/material";
 import {AuthContext} from "react-oauth2-code-pkce";
 import {useDispatch} from "react-redux";
 import {setCredentials} from "./store/authSlice.js";
+
+const ActivtiesPage = () => {
+ <Box component ="section" sx={{p:2,border:"1px solid #ccc"}}>
+ <ActivityForm onActivtiyAdded={() => window.location.reload}/>
+<AcrivtyList/>
+</Box>
+}
+
 
 function App() {
 const {token,tokenData,logIn,logOut,isAuthenticated}=useContext(AuthContext);
@@ -14,7 +22,7 @@ const dispatch=useDispatch();
 const [authReady,setAuthReady]=useState(false);
     useEffect(() => {
         if(token){
-            dispatch(setCredentials({token,tokenData}));
+            dispatch(setCredentials({token,user:tokenData}));
             setAuthReady(true);
         }
     }, [token,tokenData,dispatch]);
@@ -23,10 +31,23 @@ const [authReady,setAuthReady]=useState(false);
         {!token?(
       <Button variant="contained" color="#dc004e"
               onClick={() => logIn()}> Login </Button>
-            ):(<div>
-            <pre>{JSON.stringify(tokenData,null,2)}
-            </pre>
-            </div>
+             ):(
+             //<div>
+            // <pre>{JSON.stringify(tokenData,null,2)}
+            // </pre> // </div>
+            <Box component ="section" sx={{p:2,border:"1px solid #ccc"}}>
+              <Routes>
+                  <Route path="activities" element={<ActivtiesPage/>}/>
+                  
+
+              </Routes>
+              
+
+
+
+
+
+            </Box>
             )}
     </Router>
   )
